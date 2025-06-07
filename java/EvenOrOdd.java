206b61a94b9f4ad549595ad79799fa26c835282a
Encryption-prince <subhammaity6827@gmail.com>
2025-06-07 13:40:36 +0530

    added even odd java program

---
+++ java/EvenOrOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+        sc.close();
+    }
+}
